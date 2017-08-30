eb22d8d8ef18bf67922bcf0940807ce4cf943697
G. Wibisono <gundambison@gmail.com>
2017-08-31 01:42:03 +0700

    update branch

--- z_gundam.bat
+++ z_gundam.bat
@@ -2,10 +2,10 @@
 git fetch
 git status
 echo origin
-git pull origin live_market
-git push origin live_market -v
+git pull origin devel
+echo this is devel not live_market
+git push origin devel
+echo this is devel not live_market -v
 echo ====
+
 git status
-echo other
-git pull gitlab live_market
-git push gitlab live_market -v
